a7f0b1a0fc6a9a2dbc9c6936696fc6b4caf0203b
Diego6624 <juan2525.2004@gmail.com>
2026-09-04 18:36:10 -0500

    Test

--- src/pages/PublicSearch.jsx
+++ src/pages/PublicSearch.jsx
@@ -168,7 +168,7 @@ export default function PublicSearch() {
             </p>
 
             {/* Buscador */}
-            <form onSubmit={handleSubmit} className="mt-10 flex w-full max-w-lg flex-col gap-3 sm:flex-row">
+            <form onSubmit={handleSubmit} className="mt-10 flex w-full max-w-lg flex-col gap-4 sm:flex-row">
               <div className="relative flex-1">
                 <svg xmlns="http://www.w3.org/2000/svg" className="absolute left-4 top-1/2 h-4 w-4 -translate-y-1/2 text-slate-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2" />
